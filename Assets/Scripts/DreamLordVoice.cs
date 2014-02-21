be961db71f8b19c4ada4781497910c3594f377f4
Kevin Mcree <kevinmcree@gmail.com>
2014-02-20 20:18:32 -0800

    readied basic voice script

---
+++ Assets/Scripts/DreamLordVoice.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DreamLordVoice : MonoBehaviour {
+	public AudioClip[] clips = new AudioClip[18];
+	private int rand;
+	private int flip = 0;
+	private AudioSource[] audioSources = new AudioSource[18];
+	private bool running = false;
+	public int delay = 60;
+	private int storage =0;
+
+	// Use this for initialization
+	void Start () {
+		storage = delay;
+		rand = Random.Range (0,9);
+		int i = 0;
+		while (i < 18) {
+			GameObject child = new GameObject("audio");
+			child.transform.parent = gameObject.transform;
+			audioSources[i] = child.AddComponent("AudioSource") as AudioSource;
+			i++;
+		}
+		running = true;
+		audioSources[flip].clip = clips[flip];
+		audioSources[flip].Play();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (audioSources[flip].isPlaying)
+		return;
+	
+	if (delay==0){
+		flip++;
+		audioSources[flip].clip = clips[flip];
+		audioSources[flip].Play();		
+		delay = storage;
+			if (flip==17){
+				flip=0;
+			}
+	}else{
+		delay--;
+	}
+
+	}
+}
