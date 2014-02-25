928a9d74d9445d644e5d64b1229156de2c7b48e4
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-02-25 13:45:56 -0800

    Added background parallax scrolling
    Background is in color, but it seems to pop up more. So far we only
    have two layers (middle ground and background) but we could add more
    depending on what we consider.

---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+
+	public float scrollSpeed = 0;
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * scrollSpeed;
+		renderer.material.mainTextureOffset = new Vector2(0, offset % 1);
+	}
+}
