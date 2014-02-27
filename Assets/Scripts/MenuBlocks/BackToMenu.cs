6c23160a4dbaa4c497aead723921a33a94d4c35c
kevinmcree <kevinmcree@gmail.com>
2014-02-26 16:43:37 -0800

    Added Main Menu and instructions.

---
+++ Assets/Scripts/MenuBlocks/BackToMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMenu : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown(){
+		Camera.main.transform.position = new Vector3(5, -1, -10);
+	}
+}
+
---
+++ Assets/Scripts/MenuBlocks/InstructionBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionBlock : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown(){
+		Camera.main.transform.position = new Vector3(-13, -1, -10);
+	}
+}
