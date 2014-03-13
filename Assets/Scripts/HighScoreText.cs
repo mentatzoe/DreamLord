8a0c01a9620d7f0ae47bb9775f4f55d36a524c8e
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-03-13 12:09:35 -0700

    Tweaked death screen
    polishing.

--- Assets/Scripts/HighScoreText.cs
+++ Assets/Scripts/HighScoreText.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class HighScoreText : MonoBehaviour {
 
+	private string[] sentences = new string[10]{"Do you know the terror of he who falls asleep?",
+		"Why follow your dreams when you can follow your nightmares?",
+		"Memories can be your best dreams or your worse nightmares.",
+		"...To dream those dreams until the day we live them.",
+		"I'm suddenly finding it hard to know the difference between nightmares and consciousness",
+		"They've promised dreams can come true- but forget to mention that dreams are nightmares, too",
+		"You don't have to sleep, you don't have to dream, to have nightmares.",
+		"Are you sure you're awake?",
+		"The real nightmare begins when you wake up.",
+		"What do monsters have nightmares about?"};
+
 	// Use this for initialization
 	void Start () {
     }
@@ -10,7 +21,7 @@ public class HighScoreText : MonoBehaviour {
 	// Update is called once per frame
     void Update()
     {
-        string ScoreText = "High Score:" + PlayerPrefs.GetInt("High Score");
+        string ScoreText = sentences[GameManager.score%10]+ "\n" + PlayerPrefs.GetInt("High Score");
         guiText.text = ScoreText;
     }
 
